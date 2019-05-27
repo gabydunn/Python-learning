637be32105ad59b53765f2941297062e56852c5c
Gabriella Dunn <gdunn6@nait.ca>
2019-05-26 22:19:35 -0600

    Adding views

--- ATBS/test.py
+++ ATBS/test.py
@@ -28,7 +28,13 @@ grid = [['.', '.', '.', '.', '.', '.'],
         ['O', 'O', 'O', 'O', '.', '.'],
         ['.', 'O', 'O', '.', '.', '.'],
         ['.', '.', '.', '.', '.', '.']]
-
+for i in range(len(grid[0])):
+    for j in range(len(grid)):
+        endOfLine = (len(grid))
+        if j >= (endOfLine -1):
+            print(grid[j][i])
+        else:
+            print(grid[j][i],end="")
 #You can think of grid[x][y] as being the character at the x- and y-coordinates of a “picture” drawn with text characters. The (0, 0) origin will be in the upper-left corner, the x-coordinates increase going right, and the y-coordinates increase going down.
 
 #Copy the previous grid value, and write code that uses it to print the image.
